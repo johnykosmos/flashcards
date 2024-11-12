c5937f981f49395122f15ea5d7ec63fa453c62b5
johnykosmos <kumakpospolity@gmail.com>
2024-11-12 11:29:49 +0100

    Feat: Added getData and deleteData requests

--- app/static/src/requestHandler.js
+++ app/static/src/requestHandler.js
@@ -1,6 +1,6 @@
 
 
-export function postRequest(form, callback){
+export function formPostRequest(form, callback){
     const formData = new FormData(form);
 
     fetch(form.action, {
@@ -25,7 +25,7 @@ export function postRequest(form, callback){
     return true;
 }
 
-export function deleteRequest(form, callback){
+export function formDeleteRequest(form, callback){
     const formData = new FormData(form);
 
     fetch(form.action, {
@@ -37,6 +37,44 @@ export function deleteRequest(form, callback){
                 throw new Error(`Error ${response.status}: ${response.statusText}`);
             else{
                 if(callback) callback(formData); 
+            }
+            return response.json();
+        })
+        .then(data => {
+            //alert(data.message);
+        })
+        .catch(error => {
+            alert(error.message);
+            return false;
+        });
+
+    return true;
+}
+
+export async function getDataRequest(action){
+    try{
+        const response = await fetch(action);
+        if(!response.ok)
+            throw new Error(`Error ${response.status}: ${response.statusText}`);
+
+        const data = await response.json();
+        return data;
+    }catch(error){
+        alert(error.message);
+        return null;
+    }
+}
+
+export function removeDataRequest(action, callback){
+    fetch(action, {
+            method: "DELETE",
+            headers: {"Content-Type":"application/json"}
+        })
+        .then(response => {
+            if(!response.ok)
+                throw new Error(`Error ${response.status}: ${response.statusText}`);
+            else{
+                if(callback) callback(); 
             }
             return response.json();
         })
